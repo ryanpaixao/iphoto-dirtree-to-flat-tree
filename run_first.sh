7337e8456b7868acc97bc4877b37066aa3764b3d
ryanpaixao <ryanpaixao@gmail.com>
2018-02-07 15:29:44 +0300

    Add run_first.sh. Allow permissions for all scripts.

---
+++ run_first.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+########################################
+# script to give other files permission
+########################################
+
+sudo chmod 755 ./convert_files.sh ./testing_scripts/create_directory_tree.sh ./testing_scripts/delete_directory_tree.sh
